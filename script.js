33b6aaa4549cd1c9a132cd3c1c79c6b9cc6e49f3
Richard Uggelberg <Richard.Uggelberg@samhall.se>
2023-04-05 20:58:59 +0200

    record simple move order

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+//constants
+
+const unitSquareSize = 20;
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const graphData = {
+const map = {
   "vertices": [
-    { "id": 0, "label": "A", "x": 100, "y": 100 },
-    { "id": 1, "label": "B", "x": 200, "y": 200 },
-    { "id": 2, "label": "C", "x": 300, "y": 300 }
+    { "id": 0, "name": "A", "x": 100, "y": 100, "type": "land"},
+    { "id": 1, "name": "B", "x": 200, "y": 200, "type": "land"},
+    { "id": 2, "name": "C", "x": 300, "y": 100, "type": "land"}
   ],
   "edges": [
     { "from": 0, "to": 1 },
@@ -13,13 +17,27 @@ const graphData = {
   ]
 };
 
-function drawGraph(graph) {
-  const vertexRadius = 20;
+let playerCountry = "GER";
 
+const initialState = {
+    "units": [
+        { "country": "GER", "type": "army", "vertexId": 0}
+    ]
+}
+
+let state = initialState;
+
+const vertexSideLength = 40;
+
+let orders = [];
+
+//functions
+
+function drawMap(map) {
   // Draw edges
-  for (let edge of graph.edges) {
-    const fromVertex = graph.vertices.find(vertex => vertex.id === edge.from);
-    const toVertex = graph.vertices.find(vertex => vertex.id === edge.to);
+  for (let edge of map.edges) {
+    const fromVertex = map.vertices.find(vertex => vertex.id === edge.from);
+    const toVertex = map.vertices.find(vertex => vertex.id === edge.to);
     ctx.beginPath();
     ctx.moveTo(fromVertex.x, fromVertex.y);
     ctx.lineTo(toVertex.x, toVertex.y);
@@ -27,19 +45,103 @@ function drawGraph(graph) {
   }
 
   // Draw vertices
-  for (let vertex of graph.vertices) {
-    ctx.beginPath();
-    ctx.arc(vertex.x, vertex.y, vertexRadius, 0, 2 * Math.PI);
-    ctx.fillStyle = '#ffffff';
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
+  for (let vertex of map.vertices) {
+    ctx.strokeStyle = 'black'; // set stroke color to black
+    ctx.lineWidth = 2; // set stroke width to 2 pixels
+    ctx.strokeRect(vertex.x - vertexSideLength/2, vertex.y - vertexSideLength/2, vertexSideLength, vertexSideLength); // draw a rectangular border
     ctx.font = "bold 14px Arial";
     ctx.fillStyle = "#000000";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    ctx.fillText(vertex.label, vertex.x, vertex.y);
+    ctx.fillText(vertex.name, vertex.x, vertex.y-25);
   }
 }
 
-drawGraph(graphData);
+function drawUnits() {
+    var changes = getChanges();
+
+    for (let unit of state.units) {
+        for (let vertex of map.vertices) {
+            if (vertex.id === unit.vertexId) {
+                let size = 20;
+                ctx.fillStyle = "red";
+                ctx.fillRect(vertex.x-unitSquareSize/2, vertex.y-unitSquareSize/2, unitSquareSize, unitSquareSize);
+            }
+        }
+    }
+}
+
+function getChanges() {
+    var hash = window.location.hash;
+    if (hash) {
+        var hashSplit = hash.split('#')[1].split('/');
+
+        for (var change of hashSplit) {
+            var changeSplit = change.split('.');
+    
+        }
+    }
+}
+
+function getClickedVertexId(mouseX, mouseY) {
+    for (let vertex of map.vertices) {
+        if ((vertex.x - vertexSideLength/2) < mouseX && mouseX < (vertex.x + vertexSideLength/2) && (vertex.y - vertexSideLength/2) < mouseY && mouseY < (vertex.y + vertexSideLength/2)) {
+            return vertex.id;
+        }
+    }
+}
+
+function getFriendlyUnitOnVertex(vertexId) {
+    for (let unit of state.units) {
+        if (unit.vertexId === vertexId && unit.country === playerCountry) {
+            return unit;
+        }
+    }
+    return false;
+}
+
+function selectCountry(countryName) {
+    playerCountry = countryName;
+}
+
+function areAdjacent(vertexA, vertexB) {
+    for (let vertexPair of map.edges) {
+        if ((vertexPair.from === vertexA && vertexPair.to === vertexB) || (vertexPair.to === vertexA && vertexPair.from === vertexB)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+drawMap(map);
+drawUnits();
+
+window.onhashchange = function() {
+    // Handle the URL hash change event here
+    console.log("The URL hash has changed to: " + window.location.hash);
+};
+
+let action = "";
+let actionVertex = -1;
+
+canvas.addEventListener('click', function(event) {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    let vertexId = getClickedVertexId(mouseX, mouseY);
+    let friendlyUnit = getFriendlyUnitOnVertex(vertexId)
+    if (friendlyUnit) {
+        action = friendlyUnit.country + ": " + friendlyUnit.type + " ";
+        actionVertex = friendlyUnit.vertexId;
+    }
+    else if (action ) {
+        if (areAdjacent(actionVertex, vertexId)) {
+            action += actionVertex + " -> " + vertexId;
+            document.getElementById("ordersString").innerText += action + "\n";
+        }
+
+        action = "";
+        actionVertex = -1;
+    }
+});
